8ee9e898f4210caac0881037f82c923fe81f0be7
disgyze <brassiere@live.ru>
2021-07-08 14:25:30 +0300

    Tab fixes

--- Echo.Xmpp.Client/XmppCoreClient.cs
+++ Echo.Xmpp.Client/XmppCoreClient.cs
@@ -12,170 +12,170 @@ using Echo.Xmpp.Parser;
 
 namespace Echo.Xmpp.Client
 {
-	public delegate void XmppStanzaCallback(XmppStanza stanza);
+    public delegate void XmppStanzaCallback(XmppStanza stanza);
 
-	public class XmppCoreClient
-	{
-		public const int DefaultPort = 5222;
+    public class XmppCoreClient
+    {
+        public const int DefaultPort = 5222;
 
-		bool disposed = false;
-		const string streamFormat =
-			@"<?xml version='1.0' encoding='utf-8'?>
+        bool disposed = false;
+        const string streamFormat =
+            @"<?xml version='1.0' encoding='utf-8'?>
 				<stream:stream
 					to='{0}'
 					version='1.0'
 					xml:lang='en'
 					xmlns='jabber:client'
 					xmlns:stream='http://etherx.jabber.org/streams'>";
-		const string streamClose = "</stream:stream>";
+        const string streamClose = "</stream:stream>";
 
-		public string Host
-		{
-			get;
-			protected set;
-		}
+        public string Host
+        {
+            get;
+            protected set;
+        }
 
-		public int Port
-		{
-			get;
-			protected set;
-		}
+        public int Port
+        {
+            get;
+            protected set;
+        }
 
-		public IXmppParser Parser
-		{
-			get;
-			protected set;
-		}
+        public IXmppParser Parser
+        {
+            get;
+            protected set;
+        }
 
-		public ClientSocket Socket
-		{
-			get;
-			protected set;
-		}
+        public ClientSocket Socket
+        {
+            get;
+            protected set;
+        }
 
-		public bool IsConnected => Socket?.State == ClientSocketState.Connected;
-		public bool IsConnecting => Socket?.State == ClientSocketState.Connecting;
-		public bool IsEncrypted => Socket != null && Socket.IsEncrypted;
-		public bool IsEncrypting => Socket != null && Socket.IsEncrypting;
-		public bool IsAuthenticated { get; private set; }
+        public bool IsConnected => Socket?.State == ClientSocketState.Connected;
+        public bool IsConnecting => Socket?.State == ClientSocketState.Connecting;
+        public bool IsEncrypted => Socket != null && Socket.IsEncrypted;
+        public bool IsEncrypting => Socket != null && Socket.IsEncrypting;
+        public bool IsAuthenticated { get; private set; }
 
-		public event EventHandler<XmppElementEventArgs<XElement>> XmlElementReceived;
-		public event EventHandler<XmppElementEventArgs<XElement>> XmlElementSent;
+        public event EventHandler<XmppElementEventArgs<XElement>> XmlElementReceived;
+        public event EventHandler<XmppElementEventArgs<XElement>> XmlElementSent;
 
-		protected void OnElementReceived(XmppElementEventArgs<XElement> e)
-		{
+        protected void OnElementReceived(XmppElementEventArgs<XElement> e)
+        {
             XmlElementReceived?.Invoke(this, e);
         }
 
-		protected void OnElementSent(XmppElementEventArgs<XElement> e)
-		{
+        protected void OnElementSent(XmppElementEventArgs<XElement> e)
+        {
             XmlElementSent?.Invoke(this, e);
         }
 
-		public XmppCoreClient(ClientSocket socket, IXmppParser parser)
-		{
-			if (socket == null)
-			{
-				throw new ArgumentNullException("socket");
-			}
+        public XmppCoreClient(ClientSocket socket, IXmppParser parser)
+        {
+            if (socket == null)
+            {
+                throw new ArgumentNullException("socket");
+            }
 
-			if (parser == null)
-			{
-				throw new ArgumentNullException("parser");
-			}
+            if (parser == null)
+            {
+                throw new ArgumentNullException("parser");
+            }
 
-			//DnsEndPoint endPoint = socket.RemoteEndPoint as DnsEndPoint;
-			//Host = endPoint.Host;
-			//Port = endPoint.Port;
-			Parser = parser;
-			Parser.ElementParsed += (s, e) => OnElementReceived(new XmppElementEventArgs<XElement>(e.Element));
-			Socket = socket;
-			Socket.DataReceived += (s, e) =>
-			{
-				parser.Parse(e.Data);
-			};
-		}
+            //DnsEndPoint endPoint = socket.RemoteEndPoint as DnsEndPoint;
+            //Host = endPoint.Host;
+            //Port = endPoint.Port;
+            Parser = parser;
+            Parser.ElementParsed += (s, e) => OnElementReceived(new XmppElementEventArgs<XElement>(e.Element));
+            Socket = socket;
+            Socket.DataReceived += (s, e) =>
+            {
+                parser.Parse(e.Data);
+            };
+        }
 
 
         ~XmppCoreClient()
-		{
-			Dispose(false);
-		}
-
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposed)
-			{
-				if (disposing)
-				{
-					if (Socket != null)
-					{
-						Socket.DisconnectAsync().GetAwaiter().GetResult();
-						Socket = null;
-					}
-
-					if (Parser != null)
-					{
-						Parser.Cancel();
-						Parser = null;
-					}
-				}
-				disposed = true;
-			}
-		}
-
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-
-		public ValueTask DisposeAsync()
-		{
-			Dispose(true);
-			return default;
-		}
-
-		public Task<bool> ConnectAsync(EndPoint endpoint, CancellationToken cancellationToken = default)
-		{
-			return Socket.ConnectAsync(endpoint, cancellationToken: cancellationToken);
-		}
-
-		public async Task<bool> DisconnectAsync(bool closeStream = true, CancellationToken cancellationToken = default)
-		{
-			if (closeStream)
-            {
-				await CloseStreamAsync(cancellationToken);
-            }
-
-			return await Socket.DisconnectAsync(cancellationToken);
-		}
-
-		public async Task<bool> SendAsync(XElement element, CancellationToken cancellationToken = default)
-		{
-			var sent = await Socket.SendAsync(Encoding.UTF8.GetBytes(element.ToString()), cancellationToken);
-
-			if (sent)
-			{
-				OnElementSent(new XmppElementEventArgs<XElement>(element));
-			}
-
-			return sent;
-		}
-
-		public Task<bool> StartStreamAsync(string domain, CancellationToken cancellationToken = default)
-		{
-			return Socket.SendAsync(Encoding.UTF8.GetBytes(string.Format(streamFormat, domain)), cancellationToken);
-		}
-
-		public Task<bool> CloseStreamAsync(CancellationToken cancellationToken = default)
         {
-			return Socket.SendAsync(Encoding.UTF8.GetBytes(streamClose), cancellationToken);
+            Dispose(false);
         }
 
-		public Task<bool> EncryptAsync(SslProtocols sslProtocols, CancellationToken cancellationToken = default)
+        protected virtual void Dispose(bool disposing)
         {
-			return Socket.UpgradeToSslAsync(sslProtocols, cancellationToken);
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (Socket != null)
+                    {
+                        Socket.DisconnectAsync().GetAwaiter().GetResult();
+                        Socket = null;
+                    }
+
+                    if (Parser != null)
+                    {
+                        Parser.Cancel();
+                        Parser = null;
+                    }
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            Dispose(true);
+            return default;
+        }
+
+        public Task<bool> ConnectAsync(EndPoint endpoint, CancellationToken cancellationToken = default)
+        {
+            return Socket.ConnectAsync(endpoint, cancellationToken: cancellationToken);
+        }
+
+        public async Task<bool> DisconnectAsync(bool closeStream = true, CancellationToken cancellationToken = default)
+        {
+            if (closeStream)
+            {
+                await CloseStreamAsync(cancellationToken);
+            }
+
+            return await Socket.DisconnectAsync(cancellationToken);
+        }
+
+        public async Task<bool> SendAsync(XElement element, CancellationToken cancellationToken = default)
+        {
+            var sent = await Socket.SendAsync(Encoding.UTF8.GetBytes(element.ToString()), cancellationToken);
+
+            if (sent)
+            {
+                OnElementSent(new XmppElementEventArgs<XElement>(element));
+            }
+
+            return sent;
+        }
+
+        public Task<bool> StartStreamAsync(string domain, CancellationToken cancellationToken = default)
+        {
+            return Socket.SendAsync(Encoding.UTF8.GetBytes(string.Format(streamFormat, domain)), cancellationToken);
+        }
+
+        public Task<bool> CloseStreamAsync(CancellationToken cancellationToken = default)
+        {
+            return Socket.SendAsync(Encoding.UTF8.GetBytes(streamClose), cancellationToken);
+        }
+
+        public Task<bool> EncryptAsync(SslProtocols sslProtocols, CancellationToken cancellationToken = default)
+        {
+            return Socket.UpgradeToSslAsync(sslProtocols, cancellationToken);
         }
     }
 }
