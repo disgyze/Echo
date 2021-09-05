0645a910ad44d161c7aa2e1866bcc3c82858507b
disgyze <71587758+disgyze@users.noreply.github.com>
2021-09-05 13:57:20 +0300

    Delete Echo.Xmpp.Client directory

--- Echo.Xmpp.Client/Authentication/IXmppAuthenticator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Echo.Xmpp.Client.Authentication
-{
-	public interface IXmppAuthenticator
-	{
-		Task AuthenticateAsync(XmppCoreClient connection, XmppAddress address, string password);
-	}
-}
