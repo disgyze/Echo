b977e5e70020ef9d0ef3f2324550736825b60015
disgyze <brassiere@live.ru>
2021-09-07 21:30:55 +0300

    Remove CreateAccount method from IAccountManager

--- Echo.Core/User/IAccountManager.cs
+++ Echo.Core/User/IAccountManager.cs
@@ -9,9 +9,7 @@ namespace Echo.Core.User
         IAccount? GetAccount(int accountIndex);
         IAccount? GetAccount(Guid accountId);
         IAccount? GetAccount(XmppAddress accountAddress);
-        IAccount? CreateAccount(XmppAddress address, string? password = null);
         IAccount? UpdateAccount(IAccount account);
-
         void ShowAccountUI(XmppAddress accountAddress);
         void ShowAccountRegistrationUI();
     }
