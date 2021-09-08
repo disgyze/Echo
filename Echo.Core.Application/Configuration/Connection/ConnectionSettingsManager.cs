2ef7360f1f4156fd2497a45a608f398159ebc0e6
disgyze <brassiere@live.ru>
2021-09-08 04:24:03 +0300

    Remove unused types

--- Echo.Core.Application/Configuration/Connection/ConnectionSettingsManager.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Echo.Core.Configuration.Connection
-{
-    public abstract class ConnectionSettingsManager
-    {
-        public abstract ConnectionSecuritySettings GetForAccount(XmppAddress accountAddress);
-    }
-}
--- Echo.Core.Application/Configuration/User/IUserConfigurationStore.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Echo.Core.Configuration.User
-{
-    public interface IUserConfigurationStore
-    {
-        AccountSettings? Get(XmppAddress accountAddress);
-        AccountSettings? GetOrCreate(XmppAddress accountAddress);
-    }
-}
